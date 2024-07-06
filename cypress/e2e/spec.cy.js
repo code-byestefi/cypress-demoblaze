177ab888ee87395ee98b487ac4917a97409efafc
Estefi Turin <estefi.turin@gmail.com>
2024-07-06 12:52:51 -0300

    login test solved

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,15 @@ import { LoginMethods } from "./pages/login/login.methods"
 
 describe('template spec', () => {
   it('passes', () => {
+    const username = 'username';
+    const password = 'password';
+
     cy.visit('https://www.demoblaze.com/')
     cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login('miUsuario', 'password123')
+
+    LoginMethods.login(username, password)
+
+    cy.get('a#nameofuser').should('contain.text', username)
     cy.wait(5000)
   })
 })
